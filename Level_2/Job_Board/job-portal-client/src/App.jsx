9a4168ff84625a588bd20024883e0268b477acf5
Amahamed <ashwaqmahamed1@gmail.com>
2024-07-01 15:10:37 +0200

    setup project done

---
+++ Level_2/Job_Board/job-portal-client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <nav>Nav Bar</nav>
+    <Outlet/>
+    </>
+  )
+}
+
+export default App
